37e8fd4082f545e4352f3f33a51ba28d7a40afb6
florianmaxim <hello@florianmaxim.com>
2018-03-07 22:40:37 +0100

    was geht ab

--- src/public/components/component-outer.js
+++ src/public/components/component-outer.js
@@ -3,21 +3,15 @@ import * as config from '../../../config.json';
 import styled from 'styled-components';
 
 export default styled.div`
-  
-  margin: 0;
-  padding: 0;
-  
   width: 100vw;
-  min-height: 100vh;
+  height: 100vh;
 
   display: flex;
   align-items: center;
   justify-content: flex-start;
   flex-direction: column;
 
-  box-shadow: 0px 0px 50px rgba(255, 255, 255, 1);  
-
+  box-shadow: 0px 0px 50px rgba(255, 255, 255, 1);
+  
   box-sizing: border-box;
-  border: ${config.dev.helperBorder} solid gold;
-
 `;
