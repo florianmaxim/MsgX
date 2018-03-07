44ef0f3a8914658de2b6858c7b123ba7f8ac9bed
florianmaxim <hello@florianmaxim.com>
2018-03-07 22:15:58 +0100

    Changed notifications style

--- src/public/containers/container-message.js
+++ src/public/containers/container-message.js
@@ -11,20 +11,6 @@ import ComponentButton from '../components/component-button';
 import ComponentLogo   from '../components/component-logo';
 
 import styled from 'styled-components';
-import { setTimeout } from 'timers';
-
-function getStringFromTimestamp(UNIX_timestamp){
-  var a = new Date(UNIX_timestamp * 1000);
-  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-  var year = a.getYear();
-  var month = months[a.getMonth()];
-  var date = a.getDate();
-  var hour = a.getHours();
-  var min = a.getMinutes();
-  var sec = a.getSeconds();
-  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-  return time;
-}
 
 const Outer = styled.div`
   width: 100vw;
