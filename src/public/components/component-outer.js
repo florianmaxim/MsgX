9edc19d0a4a04e9a81acc648152f9e0117408e38
florianmaxim <hello@florianmaxim.com>
2018-03-07 22:30:56 +0100

    Outsourced component

---
+++ src/public/components/component-outer.js
@@ -0,0 +1,18 @@
+import * as config from '../../../config.json';
+
+import styled from 'styled-components';
+
+export default styled.div`
+    width: 100vw;
+    min-height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: column;
+
+    box-shadow: 0px 0px 50px rgba(255, 255, 255, 1);  
+
+    box-sizing: border-box;
+    border: ${config.dev.helperBorder} solid gold;
+`;
--- src/public/containers/container-message.js
+++ src/public/containers/container-message.js
@@ -14,21 +14,6 @@ import ComponentLogo   from '../components/component-logo';
 
 import styled from 'styled-components';
 
-const Outer = styled.div`
-  width: 100vw;
-  min-height: 100vh;
-
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  flex-direction: column;
-
-  box-shadow: 0px 0px 50px rgba(255, 255, 255, 1);  
-
-  box-sizing: border-box;
-  border: ${config.dev.helperBorder} solid gold;
-`;
-
 const ComponentNotificationsWrapper = styled.div`
 
   transition: 0.5s all;
