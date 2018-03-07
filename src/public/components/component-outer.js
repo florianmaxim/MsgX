5da15e5d34b0041145097b26985c69b56c8330b1
florianmaxim <hello@florianmaxim.com>
2018-03-07 22:48:23 +0100

    merge

--- src/public/components/component-outer.js
+++ src/public/components/component-outer.js
@@ -3,15 +3,21 @@ import * as config from '../../../config.json';
 import styled from 'styled-components';
 
 export default styled.div`
+
   width: 100vw;
   height: 100vh;
+
+  
+  margin: 0;
+  padding: 0;
+  
 
   display: flex;
   align-items: center;
   justify-content: flex-start;
   flex-direction: column;
 
-  box-shadow: 0px 0px 50px rgba(255, 255, 255, 1);
-  
   box-sizing: border-box;
+  border: ${config.dev.helperBorder} solid gold;
+
 `;
